0d06f4e44afe8045437a66d9c6f969db02e53287
Tomasz Kot <tomasz.kot@qualtronic.eu>
2015-05-13 13:39:34 +0200

    Taby OK

---
+++ app/src/main/java/com/example/qualtronic/tabinactionbar/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.qualtronic.tabinactionbar;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainActivity extends FragmentActivity implements
+        ActionBar.TabListener {
+
+    public Fragment myFragmentTemp;
+
+
+    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+/////////    ACTON BAR  ----------------------------------------------------------------------------
+        // Set up the action bar to show tabs.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // for each of the sections in the app, add a tab to the action bar.
+        actionBar.addTab(actionBar.newTab().setText("ULUBIONE")
+                .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("PUNKTY SWIETLNE")
+                .setTabListener(this));
+///--------------------------------------------------------------------------------------------------
+
+
+
+
+        if (savedInstanceState == null){
+
+            myFragmentTemp = new Fragment1();
+
+            //ft.add(R.id.content_layout, myFragment);
+           //myFragment.setArguments(myBundle);   //Musi byæ do przesy³ania bundle
+            // Log.d("Test", "BUNDLE SEND________" + myBundle );
+           // ft.commit();
+        }
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore the previously serialized current tab position.
+        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+            getActionBar().setSelectedNavigationItem(savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // Serialize the current tab position.
+        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
+                .getSelectedNavigationIndex());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab,
+                              FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, show the tab contents in the
+        // container view.
+/*        android.app.Fragment fragment = new DummySectionFragment();
+        Bundle args = new Bundle();
+        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER,
+                tab.getPosition() + 1);
+        fragment.setArguments(args);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment).commit();*/
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab,
+                                FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab,
+                                FragmentTransaction fragmentTransaction) {
+    }
+}
