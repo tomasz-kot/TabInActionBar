a01773b809f05f43fe59978c67d8cc5fda1e89d6
Tomasz Kot <tomasz.kot@qualtronic.eu>
2015-05-13 13:27:18 +0200

    Taby OK

---
+++ app/src/main/java/com/example/qualtronic/tabinactionbar/Fargment1.java
@@ -0,0 +1,17 @@
+package com.example.qualtronic.tabinactionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tomasz on 11/05/2015.
+ */
+public class Fargment1 extends Fragment{
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}
